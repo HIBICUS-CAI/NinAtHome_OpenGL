4ed99732ce15b0bb015ae39e0f944e1f2595bd58
caigenkan <cai_genkan@outlook.com>
2021-06-10 15:17:09 +0900

    a strange warning

--- NinAtHome/BasicInit_LowLevel/LowLevelCpp.cpp
+++ NinAtHome/BasicInit_LowLevel/LowLevelCpp.cpp
@@ -5,57 +5,58 @@ GlHelper* gp_GlHelper = nullptr;
 
 bool InitSystem(int width, int height, const char* title)
 {
-	if (!InitGlAndCreateWindow(width,height,title))
-	{
-		std::cout << "failed to init basic" << std::endl;
-		return false;
-	}
+    if (!InitGlAndCreateWindow(width, height, title))
+    {
+        std::cout << "failed to init basic" << std::endl;
+        return false;
+    }
 
-	gp_GlHelper = new GlHelper();
-	if (!gp_GlHelper->StartUp())
-	{
-		std::cout << "failed to init gl" << std::endl;
-		delete gp_GlHelper;
-		gp_GlHelper = nullptr;
-		return false;
-	}
+    gp_GlHelper = new GlHelper();
+    if (!gp_GlHelper->StartUp())
+    {
+        std::cout << "failed to init gl" << std::endl;
+        delete gp_GlHelper;
+        gp_GlHelper = nullptr;
+        return false;
+    }
 
-	return true;
+    return true;
 }
 
 void UninitSystem()
 {
-	TerminateGLFW();
+    TerminateGLFW();
 }
 
 void SwapBuffers()
 {
-	GoRunLoopProcess();
-	gp_GlHelper->ClearVaoVbo();
+    GoRunLoopProcess();
+    gp_GlHelper->ClearVaoVbo();
 
-	Sleep(15);
+    Sleep(15);
 }
 
 bool ShouldQuit()
 {
-	return ShouldGlfwQuit();
+    return ShouldGlfwQuit();
 }
 
 GlHelper* GetGlHelperPtr()
 {
-	return gp_GlHelper;
+    return gp_GlHelper;
 }
 
 void SetVertexAttr(const VERTEX* v, int arraySize)
 {
-	float* buffer = new float[9 * arraySize];
-	GLuint* vao = new GLuint;
-	GLuint* vbo = new GLuint;
-	GetGlHelperPtr()->MoveDataToBuffer(
-		v, arraySize, buffer);
-	GetGlHelperPtr()->BindVAOWithVBO(vao, vbo, buffer,
-		9 * arraySize);
-	glBindVertexArray(*vao);
-	delete vao, vbo;
-	delete[] buffer;
+    arraySize *= 9;
+    float* buffer = new float[arraySize];
+    GLuint* vao = new GLuint;
+    GLuint* vbo = new GLuint;
+    GetGlHelperPtr()->MoveDataToBuffer(
+        v, arraySize, buffer);
+    GetGlHelperPtr()->BindVAOWithVBO(vao, vbo, buffer,
+        9 * arraySize);
+    glBindVertexArray(*vao);
+    delete vao, vbo;
+    delete[] buffer;
 }
