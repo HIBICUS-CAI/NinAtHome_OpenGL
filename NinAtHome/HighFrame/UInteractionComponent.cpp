afe1b1317d1a4fa544b0fe7ab6d7670dbed055b4
caigenkan <cai_genkan@outlook.com>
2021-06-10 20:12:58 +0900

    add the defination about UIINTERACTION

---
+++ NinAtHome/HighFrame/UInteractionComponent.cpp
@@ -0,0 +1,71 @@
+//---------------------------------------------------------------
+// File: UInteractionComponent.cpp
+// Proj: NinAtHome
+// Info: UIオブジェクトに自身特有に関してのコンポーネント
+// Date: 2021.06.10
+// Mail: cai_genkan@outlook.com
+// Comt: NULL
+//---------------------------------------------------------------
+
+#include "UInteractionComponent.h"
+#include "UiObject.h"
+
+UInteractionComponent::UInteractionComponent(std::string _name,
+    UiObject* _owner, int _order) :
+    UComponent(_name, _owner, _order), mInterInitFuncPtr(nullptr),
+    mInterUpdateFuncPtr(nullptr), mInterDestoryFuncPtr(nullptr)
+{
+
+}
+
+UInteractionComponent::~UInteractionComponent()
+{
+
+}
+
+void UInteractionComponent::CompInit()
+{
+
+}
+
+void UInteractionComponent::CompUpdate(float _deltatime)
+{
+
+}
+
+void UInteractionComponent::CompDestory()
+{
+
+}
+
+void UInteractionComponent::SetInitFunc(UiInterInitFuncType _func)
+{
+    mInterInitFuncPtr = _func;
+}
+
+void UInteractionComponent::SetUpdateFunc(
+    UiInterUpdateFuncType _func)
+{
+    mInterUpdateFuncPtr = _func;
+}
+
+void UInteractionComponent::SetDestoryFunc(
+    UiInterDestoryFuncType _func)
+{
+    mInterDestoryFuncPtr = _func;
+}
+
+void UInteractionComponent::ClearInitFunc()
+{
+    mInterInitFuncPtr = nullptr;
+}
+
+void UInteractionComponent::ClearUpdateFunc()
+{
+    mInterUpdateFuncPtr = nullptr;
+}
+
+void UInteractionComponent::ClearDestoryFunc()
+{
+    mInterDestoryFuncPtr = nullptr;
+}
---
+++ NinAtHome/HighFrame/UInteractionComponent.h
@@ -0,0 +1,54 @@
+//---------------------------------------------------------------
+// File: UInteractionComponent.h
+// Proj: NinAtHome
+// Info: UIオブジェクトに自身特有に関してのコンポーネント
+// Date: 2021.06.10
+// Mail: cai_genkan@outlook.com
+// Comt: NULL
+//---------------------------------------------------------------
+
+#pragma once
+
+#include "UComponent.h"
+
+// TEMP-----------------
+using UiInterInitFuncType = void(*)();
+using UiInterUpdateFuncType = void(*)();
+using UiInterDestoryFuncType = void(*)();
+// TEMP-----------------
+
+class UInteractionComponent :
+    public UComponent
+{
+public:
+    UInteractionComponent(std::string _name,
+        class UiObject* _owner, int _order);
+    virtual ~UInteractionComponent();
+
+    void SetInitFunc(UiInterInitFuncType _func);
+
+    void SetUpdateFunc(UiInterUpdateFuncType _func);
+
+    void SetDestoryFunc(UiInterDestoryFuncType _func);
+
+    void ClearInitFunc();
+
+    void ClearUpdateFunc();
+
+    void ClearDestoryFunc();
+
+public:
+    virtual void CompInit();
+
+    virtual void CompUpdate(float _deltatime);
+
+    virtual void CompDestory();
+
+private:
+    UiInterInitFuncType mInterInitFuncPtr;
+
+    UiInterUpdateFuncType mInterUpdateFuncPtr;
+
+    UiInterDestoryFuncType mInterDestoryFuncPtr;
+};
+
